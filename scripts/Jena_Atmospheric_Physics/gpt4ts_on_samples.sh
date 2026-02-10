d8d70b43dce6b977aab3d3d3ac6b782066814157
standaloneosc <oscardmelendez7@gmail.com>
2026-02-10 01:46:10 -0500

    exp layout

--- scripts/Jena_Atmospheric_Physics/gpt4ts_on_samples.sh
+++ scripts/Jena_Atmospheric_Physics/gpt4ts_on_samples.sh
@@ -19,3 +19,4 @@ python -u criterias.py \
     --batch_size 1 \
     --device "2" \
     --filtered_samples "sample_indexes/Jena_Atmospheric_Physics_sample_week.json" | tee -a ./logs/test_RPLLM_on_samples.log
+# fork-trace:629a41ff
