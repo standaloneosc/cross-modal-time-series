11a3ccf421aac8789ffce51bf3a8b8aa963721e7
standaloneosc <oscardmelendez7@gmail.com>
2026-02-26 18:47:56 -0500

    electricity yaml

--- scripts/run_all_IATSF.sh
+++ scripts/run_all_IATSF.sh
@@ -4,3 +4,4 @@ bash scripts/Germany_Renewable_Power_Grid/TGTSF.sh
 bash scripts/Jena_Atmospheric_Physics/TGTSF.sh
 bash scripts/NYC_traffic_speed/TGTSF.sh
 bash scripts/Bear_room/TGTSF.sh
+# fork-trace:f7105de2
