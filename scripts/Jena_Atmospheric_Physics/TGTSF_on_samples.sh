428368911aa085bd81c073a1ff2aa04835f96e5d
standaloneosc <oscardmelendez7@gmail.com>
2026-03-01 13:38:14 -0500

    tools helper

--- scripts/Jena_Atmospheric_Physics/TGTSF_on_samples.sh
+++ scripts/Jena_Atmospheric_Physics/TGTSF_on_samples.sh
@@ -19,3 +19,4 @@ python -u criterias_lightning.py \
     --batch_size 1 \
     --device "3" \
     --filtered_samples "sample_indexes/Jena_Atmospheric_Physics_sample_week.json" | tee -a ./logs/test_IATSF_on_samples.log
+# fork-trace:7004ddf5
--- scripts/run_all_IATSF_m.sh
+++ scripts/run_all_IATSF_m.sh
@@ -2,3 +2,4 @@ bash scripts/California_ISO/TGTSF_m.sh
 bash scripts/Germany_Renewable_Power_Grid/TGTSF_m.sh
 bash scripts/Jena_Atmospheric_Physics/TGTSF_m.sh
 bash scripts/NYC_traffic_speed/TGTSF_m.sh
+# fork-trace:7004ddf5
